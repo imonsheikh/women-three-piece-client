2ab8a96f7284029406d009b2dad4c7acb7005362
imon-sheikh <mdimonhossain2017@gmail.com>
2025-03-14 15:37:23 +0600

    modify categories

--- src/pages/Home/ShopByCategories/ShopByCategories.jsx
+++ src/pages/Home/ShopByCategories/ShopByCategories.jsx
@@ -7,20 +7,20 @@ const ShopByCategories = () => {
             <SectionTitle heading={'Shop By Categories'}></SectionTitle>
             <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3'>
               <div>
-               <img className='rounded-2xl h-50 w-full object-cover brightness-50' src="https://www.smarteshopbd.com/wp-content/uploads/2025/03/DRS87.webp" alt="" />
-               <h1 className='font-bold text-xl text-center'>Secuence</h1>
+               <img className='rounded-t-2xl h-50 w-full object-cover brightness-50' src="https://www.smarteshopbd.com/wp-content/uploads/2025/03/DRS87.webp" alt="" />
+               <h1 className='font-bold text-xl bg-primary-c rounded-b-4xl text-white text-center p-1'>Secuence</h1>
               </div>
               <div>
-               <img className='rounded-2xl h-50 w-full object-cover brightness-50' src="https://www.smarteshopbd.com/wp-content/uploads/2025/03/DRS88.webp" alt="" />
-               <h1 className='font-bold text-xl text-center'> Ari embroidery</h1>
+               <img className='rounded-t-2xl h-50 w-full object-cover brightness-50' src="https://www.smarteshopbd.com/wp-content/uploads/2025/03/DRS88.webp" alt="" />
+               <h1 className='font-bold text-xl  bg-primary-c rounded-b-4xl text-white text-center p-1'> Ari embroidery</h1>
               </div>
               <div>
-               <img className='rounded-2xl h-50 w-full object-cover brightness-50' src="https://www.smarteshopbd.com/wp-content/uploads/2025/03/DRS89.webp" alt="" />
-               <h1 className='font-bold text-xl text-center'>Malhar</h1>
+               <img className='rounded-t-2xl h-50 w-full object-cover brightness-50' src="https://www.smarteshopbd.com/wp-content/uploads/2025/03/DRS89.webp" alt="" />
+               <h1 className='font-bold text-xl bg-primary-c rounded-b-4xl text-white text-center p-1'>Malhar</h1>
               </div>
               <div>
-               <img className='rounded-2xl h-50 w-full object-cover brightness-50' src="https://www.smarteshopbd.com/wp-content/uploads/2025/03/DRS83.webp" alt="" />
-               <h1 className='font-bold text-xl text-center'>Zom Zom</h1>
+               <img className='rounded-t-2xl h-50 w-full object-cover brightness-50' src="https://www.smarteshopbd.com/wp-content/uploads/2025/03/DRS83.webp" alt="" />
+               <h1 className='font-bold text-xl bg-primary-c rounded-b-4xl text-white text-center p-1'>Zom Zom</h1>
               </div>
             
             </div>
