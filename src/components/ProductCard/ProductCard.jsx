706c9e6700d105828908ccbb6ccb4b1efd9244db
imon-sheikh <mdimonhossain2017@gmail.com>
2025-05-31 10:06:36 +0600

    before increase decrease adding loading

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -28,22 +28,32 @@ const ProductCard = ({ product }) => {
   };
 
   const increaseQty = async () => {
-    if (!cartItemId) return;
+    if (!cartItemId) return; 
+    // Optimistically update UI
+    setQuantity((prev) => prev + 1);
+    setTotalPrice((prev) => prev + productPrice);
+    
     const res = await updateQuantity(cartItemId, "increase");
-    if (res.status === 200) {
-      setQuantity((prev) => prev + 1);
-      setTotalPrice((prev) => prev + productPrice);
+    if (res.status !== 200) { 
+    // Rollback if failed
+      setQuantity((prev) => prev - 1);
+    setTotalPrice((prev) => prev - productPrice);
+    }else{
       refetch();
     }
   };
 
   const decreaseQty = async () => {
-    if (quantity <= 1 || !cartItemId) return;
+    if (quantity <= 1 || !cartItemId) return; 
+    // Optimistically update UI
+    setQuantity((prev) => prev - 1);
+    setTotalPrice((prev) => prev - productPrice);
+
     const res = await updateQuantity(cartItemId, "decrease");
     if (res.status === 200) {
-      setQuantity((prev) => prev - 1);
-      setTotalPrice((prev) => prev - productPrice);
-      refetch();
+      setQuantity((prev) => prev + 1);
+      setTotalPrice((prev) => prev + productPrice);
+      refetch();//Optional
     }
   };
 
